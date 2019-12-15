a450b68ea26231ddf42fb0e440837fb24051e262
mirdsmulya <by_ms19@yahoo.com>
2019-12-15 17:37:37 +0700

    Add generateUserName() due issue: different full name but same userName

--- emailGenerator.py
+++ emailGenerator.py
@@ -12,11 +12,14 @@ class emailGenerator(object):
         result = word.split(' ')
         return result
 
-    def oneWordName(self, words, number):
-        return words[0] + str(number) 
+    def oneWordName(self, words):
+        return words[0] 
         
-    def moreThanOneWord(self, words, number):
-        return words[0] + "." + words[-1] + str(number)
+    def moreThanOneWord(self, words):
+        return words[0] + "." + words[-1] 
+    
+    def generateUserName(self, user, number):
+        return user + str(number)
         
     def generateEmailName(self, words):
         return words + self.domain
@@ -34,22 +37,25 @@ if __name__ == "__main__":
         lowerChar = emGenerator.toLower(input)
         splittedChar = emGenerator.split(lowerChar)
 
-        if lowerChar in database:
-            number = database[lowerChar] + 1
-            database[lowerChar] = number
-        else:
-            database[lowerChar] = 0
-            number = ''
-
         lengthArray = len(splittedChar)
 
         if lengthArray == 1:
-            userName = emGenerator.oneWordName(splittedChar, number)
+            userName = emGenerator.oneWordName(splittedChar)
         else:
-            userName = emGenerator.moreThanOneWord(splittedChar, number)
+            userName = emGenerator.moreThanOneWord(splittedChar)
     
-        emailName = emGenerator.generateEmailName(userName)
 
+        if userName in database:
+            number = database[userName] + 1
+            database[userName] = number
+        else:
+            database[userName] = 0
+            number = ''
+
+        userNameFinal = emGenerator.generateUserName(userName,number)
+        emailName = emGenerator.generateEmailName(userNameFinal)
+
+        print ""
         print "Username Anda:", userName
         print "Email anda   :", emailName
         print " "
